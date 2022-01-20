fb2aa468e05e13fd8ed9a880e28bba7d26851733
quiquequique <eabramzon@gmail.com>
2022-01-20 14:53:00 -0300

    sockets working with some tests, form working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { Server as SocketServer } from 'socket.io';
 import http from 'http';
+import { v4 as uuid } from 'uuid';
+
+const notes = [];
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -10,6 +13,20 @@ app.use(express.static(`${__dirname}/public`));
 
 io.on('connection', (socket) => {
   console.log('new connection: ', socket.id);
+
+  socket.emit('ping');
+
+  socket.on('returnedPing', () => {
+    console.log('devolvio el ping: ', socket.id);
+  });
+
+  socket.on('client:newnote', (newNote) => {
+    const note = ({ ...newNote, id: uuid() });
+
+    notes.push(note);
+
+    socket.emit('server:newnote', note);
+  });
 });
 
 httpServer.listen(3000);
--- src/public/main.js
+++ src/public/main.js
@@ -1,2 +1,28 @@
 // eslint-disable-next-line no-undef
-io('http://localhost:3000');
+const socket = io('http://localhost:3000');
+
+socket.on('ping', () => {
+  console.log('listened');
+
+  socket.emit('returnedPing');
+});
+
+const noteForm = document.querySelector('#noteForm');
+const title = document.querySelector('#title');
+const description = document.querySelector('#description');
+const notes = document.querySelector('#notes');
+
+noteForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  socket.emit('client:newnote', {
+    title: title.value,
+    description: description.value,
+  });
+});
+
+socket.on('server:newnote', (data) => {
+  console.log(data);
+
+  notes.innerHTML += 'new note';
+});
